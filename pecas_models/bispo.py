07a4a8f7bd6af9ebc3534d7af4f484ce0eec85fa
Breno Froes <breno_froes@id.uff.br>
2021-03-03 18:44:09 -0300

    Ajuste de comentarios

--- pecas_models/bispo.py
+++ pecas_models/bispo.py
@@ -29,21 +29,19 @@ class Bispo(PecaBase):
         i: int = self.posicao[0]
         j: int = self.posicao[1]
 
-        #CÓDIGO
         casa_i = i
         casa_j = j
-        casa_i_j_vazia = True  # proxima casa da variável i está vazia
+        casa_i_j_vazia = True  # proxima casa da variável i/j está vazia
         while (casa_i + 1 < 8) and (casa_j + 1 < 8) and casa_i_j_vazia:  # fica em loop enquanto a próxima casa está vazia ou encontrar uma peça do outro time
             if tabuleiro[casa_i + 1][casa_j + 1].peca is None:
                 casas_possiveis.append(tabuleiro[casa_i + 1][casa_j + 1])
             elif self.tonalidade != tabuleiro[casa_i + 1][casa_j + 1].peca.tonalidade:
                 casas_possiveis.append(tabuleiro[casa_i + 1][casa_j + 1])
-                casa_i_j_vazia = False  # Próxima casa da variável i está com uma peça de cor diferente. Guarda a casa e sai do loop
+                casa_i_j_vazia = False  # Próxima casa da variável i/j está com uma peça de cor diferente. Guarda a casa e sai do loop
             else:
-                casa_i_j_vazia = False  # Próxima casa da variável i está com uma peça da mesma cor. Sai do loop
+                casa_i_j_vazia = False  # Próxima casa da variável i/j está com uma peça da mesma cor. Sai do loop
             casa_i += 1
             casa_j += 1
-            print("i1", casa_i, "j1", casa_j)
 
         casa_i = i
         casa_j = j
@@ -59,7 +57,6 @@ class Bispo(PecaBase):
                 casa_i_j_vazia = False
             casa_i -= 1
             casa_j -= 1
-            print("i2", casa_i, "j2", casa_j)
 
         casa_j = j
         casa_i = i
@@ -74,7 +71,6 @@ class Bispo(PecaBase):
                 casa_i_j_vazia = False
             casa_j -= 1
             casa_i += 1
-            print("i3", casa_i, "j3", casa_j)
 
         casa_j = j
         casa_i = i
@@ -89,6 +85,5 @@ class Bispo(PecaBase):
                 casa_i_j_vazia = False
             casa_j += 1
             casa_i -= 1
-            print("i4", casa_i, "j4", casa_j)
 
         return casas_possiveis
