51f189c0db2c7fe20acf95ea7f15d5678e19ad28
Breno Froes <breno_froes@id.uff.br>
2021-03-26 13:10:01 -0300

    Alterando imagens

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, pygame_menu
 
 from appConstants import ImagesPath
+from start import Start
 from tabuleiro import Tabuleiro
 
 # Inicializando Pygame
@@ -30,32 +31,5 @@ windowIcon = pygame.image.load(ImagesPath.ICONE_PRETO)
 pygame.display.set_caption("UFF Chess")
 pygame.display.set_icon(windowIcon)
 
-gameLoop: bool = True
-clock = pygame.time.Clock()
-
 tabuleiro = Tabuleiro(display=display)
-
-def draw():
-    display.fill([46, 46, 46])
-
-    tabuleiro.objectGroup.update()
-    tabuleiro.desenhar_tabuleiro()
-
-    pygame.display.update()
-
-
-if __name__ == '__main__':
-    while gameLoop:
-        clock.tick(30)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameLoop = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                posicao_mouse = pygame.mouse.get_pos()
-                if tabuleiro.clicou_dentro_do_tabuleiro(posicao_mouse):
-                    tabuleiro.selecionar_casa(posicao_mouse)
-                else:
-                    tabuleiro.limpar_selecoes()
-
-        draw()
+start = Start(display, tabuleiro)
