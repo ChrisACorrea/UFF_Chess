baadd742ed5085932cc258fb6ef232a1a5a9e2b1
ChrisACorrea <christopher.a.c@outlook.com>
2021-04-29 21:08:09 -0300

    Correções na IA

--- ia.py
+++ ia.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import pygame
 import random
 
+from casa import Casa
+
+
 class IA(pygame.sprite.Sprite):
     def __init__(
             self,
@@ -15,8 +18,8 @@ class IA(pygame.sprite.Sprite):
         if tabuleiro.vez != cor:
             return
         maior = 0
-        casa_destino = None
-        casa_origem = None
+        casa_destino: Casa = None
+        casa_origem: Casa = None
         jogadas_aleatorias_possiveis = []
 
         # selecionar jogada que captura a melhor peça, se houver, ou adicionar as jogadas possíveis a um vetor
@@ -24,16 +27,19 @@ class IA(pygame.sprite.Sprite):
             for j in range(8):
                 if tabuleiro.vetor_de_Controle[i][j].peca is not None:
                     peca = tabuleiro.vetor_de_Controle[i][j].peca
+                    casa_selecionada: Casa = tabuleiro.vetor_de_Controle[i][j]
                     if peca.tonalidade == tabuleiro.vez:
-                        for casa in peca.get_casas_possiveis(tabuleiro.vetor_de_Controle):
+                        tabuleiro.selecionar_casa(None, casa_selecionada.posicao_na_matriz)
+                        for casa in tabuleiro.casas_possiveis:
                             # tentando selecionar jogada que captura a melhor peça, se houver
                             if casa.peca is not None and casa.peca.tonalidade != cor:
                                 if (casa.peca.valor >= maior):
                                     maior = casa.peca.valor
                                     casa_destino = casa
                                     casa_origem = tabuleiro.vetor_de_Controle[i][j]
-                        if peca is not None and len(peca.get_casas_possiveis(tabuleiro.vetor_de_Controle)) > 0:
+                        if peca is not None and len(tabuleiro.casas_possiveis) > 0:
                             jogadas_aleatorias_possiveis.append(tabuleiro.vetor_de_Controle[i][j])
+                        tabuleiro.limpar_selecoes()
 
 
         if casa_destino is None and casa_origem is None:
@@ -43,19 +49,22 @@ class IA(pygame.sprite.Sprite):
             else:
                 tabuleiro.fim_jogo()
             casa_origem = jogadas_aleatorias_possiveis[index]
-            casa_origem.marcar_como_selecionado()
+            # casa_origem.marcar_como_selecionado()
+            tabuleiro.selecionar_casa(None, casa_origem.posicao_na_matriz)
 
-            tam_destino = len(casa_origem.peca.get_casas_possiveis(tabuleiro.vetor_de_Controle))
+            tam_destino = len(tabuleiro.casas_possiveis)
             index_destino = random.randrange(0, tam_destino, 1)
-            casa_destino = casa_origem.peca.get_casas_possiveis(tabuleiro.vetor_de_Controle)[index_destino]
+            casa_destino = tabuleiro.casas_possiveis[index_destino]
+            tabuleiro.limpar_selecoes()
 
-        casa_destino.marcar_como_possivel()    
-        tabuleiro.casa_selecionada = casa_origem    
-        tabuleiro.casas_possiveis = tabuleiro.casa_selecionada.peca.get_casas_possiveis(tabuleiro.vetor_de_Controle)
+        # casa_destino.marcar_como_possivel()
+        tabuleiro.selecionar_casa(None, casa_origem.posicao_na_matriz)
+        #tabuleiro.casa_selecionada = casa_origem
+        #tabuleiro.casas_possiveis = tabuleiro.casa_selecionada.peca.get_casas_possiveis(tabuleiro.vetor_de_Controle)
         # tabuleiro.selecionar_casa(casa_origem, casa_destino.posicao_na_matriz)
         # tabuleiro.casas_possiveis = tabuleiro.casa_selecionada.peca.get_casas_possiveis(tabuleiro.vetor_de_Controle)
-        tabuleiro.selecionar_casa(casa_destino, casa_destino.posicao_na_matriz)
-        tabuleiro.limpar_selecoes()
+        tabuleiro.selecionar_casa(None, casa_destino.posicao_na_matriz)
+        #tabuleiro.limpar_selecoes()
 
     def jogador_vs_ia(self, tabuleiro):
         self.movimento_peca(tabuleiro, "escuro")
